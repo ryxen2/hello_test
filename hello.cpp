75c3d8619fdf1a2614c59db141a4ce714cb40971
ryxen <binfile040@gmail.com>
2024-09-18 21:39:44 +0600

    python hello

---
+++ hello.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define int long long
+#define PII pair<int, int>
+#define endl "\n"
+const int MAX = 1e9;
+const int N = 200010;
+const ll INF = 1e9+7;
+
+void solve()
+{
+cout << "Hello World!";
+}
+
+signed main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int T = 1;
+    cin >> T;
+    while (T--)
+        solve();
+    return 0;
+}
